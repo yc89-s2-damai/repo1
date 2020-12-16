27b85da194f60bcfeb126f31bcd63f7783beac5e
Ella-Ming <Ella-Ming@172.20.83.8>
2020-12-16 16:09:48 +0800

    git测试

--- test01/src/main/java/test01/test1.java
+++ test01/src/main/java/test01/test1.java
@@ -2,6 +2,7 @@ package test01;
 
 public class test1 {
 public static void main(String[] args) {
-	System.out.println("new file");
+	System.out.println("abcd");
+	System.out.println("aaaaaa");
 }
 }
--- test01/test/src/main/java/testgit/testgit.java
+++ test01/test/src/main/java/testgit/testgit.java
@@ -3,5 +3,7 @@ package testgit;
 public class testgit {
 	public static void main(String[] args) {
 		System.out.println("a");
+		
+		System.out.println("abc");
 	}
 }
